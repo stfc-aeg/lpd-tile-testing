4f0fd825842eaeafef9c1bd203487e811e712197
Matthew Richards <matthew.richards@stfc.ac.uk>
2019-02-05 16:23:27 +0000

    Create PDFs in an external folder

--- generate_report.py
+++ generate_report.py
@@ -1,11 +1,15 @@
 from matplotlib.backends.backend_pdf import PdfPages
+import os
 
 
 def export(fig_list, filename):
     # Remove file extension from filename of data
     filename = filename.split('.')[0]
 
-    pdf_file = PdfPages('test_results_{}.pdf'.format(filename))
+    # save_path must not have trailing forward slash
+    save_path = "{}/develop/projects/lpd/tile_analysis".format(os.environ['HOME'])
+
+    pdf_file = PdfPages('{}/test_results_{}.pdf'.format(save_path, filename))
     for figure in fig_list:
         # Insert each figure into PDF created by Matplotlib
         figure.savefig(pdf_file, format='pdf')
