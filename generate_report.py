9a77b221e6e288bfd52a2feed1a5b80a406ff0fa
Matthew Richards <matthew.richards@stfc.ac.uk>
2019-02-05 14:02:50 +0000

    PDF filenames are now based on the filename of the data used for the analysis

--- generate_report.py
+++ generate_report.py
@@ -1,8 +1,13 @@
 from matplotlib.backends.backend_pdf import PdfPages
 
 
-def export(fig_list):
-    pdf_file = PdfPages('test_results.pdf')
+def export(fig_list, filename):
+    # Remove file extension from filename of data
+    filename = filename.split('.')[0]
+
+    pdf_file = PdfPages('test_results_{}.pdf'.format(filename))
     for figure in fig_list:
+        # Insert each figure into PDF created by Matplotlib
         figure.savefig(pdf_file, format='pdf')
+
     pdf_file.close()
